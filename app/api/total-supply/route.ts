0406f9d00d2ad183d7c3583347579c3db22a1ad3
Dark Cobra <khubairnasir26@gmail.com>
2025-06-06 21:09:58 +0100

    nbfdn

--- app/api/total-supply/route.ts
+++ app/api/total-supply/route.ts
@@ -1,13 +1,6 @@
-import { NextResponse } from 'next/server';
-
 export async function GET() {
   // The total supply is 1 billion (1,000,000,000)
   const total_supply = 1000000000;
 
-  return new NextResponse(total_supply.toString(), {
-    status: 200,
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  });
+  return Response.json(total_supply.toString());
 } 
